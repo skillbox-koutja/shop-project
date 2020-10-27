91f6d606c222625c4dc0d3f91184f4b3a00d489c
Кутякин Михаил <michakoutjakin@gmail.com>
2020-10-27 22:52:08 +0300

    Исправлена ошибка сброса фильтров при выборе сортировки

--- shop/public/js/scripts.js
+++ shop/public/js/scripts.js
@@ -39,10 +39,13 @@ const reloadHome = (state) => {
     if (state.waitEditingSorting) {
         return;
     }
+
+    const filters = getFilters();
+    const props = {...state, ...filters};
     const url = new URL(window.location.origin);
-    for (let prop in state) {
-        if (state.hasOwnProperty(prop)) {
-            url.searchParams.set(prop, state[prop]);
+    for (let prop in props) {
+        if (props.hasOwnProperty(prop)) {
+            url.searchParams.set(prop, props[prop]);
         }
     }
 
@@ -54,22 +57,22 @@ const checkboxValue = (selection) => {
         return checkbox.val();
     }
     return false;
-}
+};
 const setCheckboxState = (state, prop, selection) => {
     const value = checkboxValue(selection);
     if (value) {
         state[prop] = value;
     }
     return state;
-}
+};
 const getFilters = () => {
     const state = {};
     const sliderEl = $('.range__line');
 
     state.minPrice = sliderEl.slider('values', 0);
     state.maxPrice = sliderEl.slider('values', 1);
-    setCheckboxState(state, 'new','#new');
-    setCheckboxState(state, 'sale','#sale');
+    setCheckboxState(state, 'new', '#new');
+    setCheckboxState(state, 'sale', '#sale');
 
     return state;
 };
@@ -78,16 +81,15 @@ if (filterForm) {
     filterForm.addEventListener('submit', evt => {
         evt.preventDefault();
         const state = getState();
-        const filters = getFilters();
 
-        reloadHome({...state, ...filters});
+        reloadHome(state);
     });
 }
 
 const hideSortOptions = e => {
     const el = $(e);
     if (el.val()) {
-        return
+        return;
     }
     el.hide();
 };
@@ -107,11 +109,7 @@ if (order) {
 const toggleHidden = (...fields) => {
     fields.forEach((field) => {
         if (!field) return;
-        if (field.hidden === true) {
-            field.hidden = false;
-        } else {
-            field.hidden = true;
-        }
+        field.hidden = field.hidden !== true;
     });
 };
 
@@ -135,22 +133,22 @@ const toggleDelivery = (elem) => {
     const methods = elem.querySelectorAll('.delivery-method-type [type=radio]');
 
     const requiredToggle = input => {
-        input = $(input)
-        input.prop('required', !input.prop('required'))
+        input = $(input);
+        input.prop('required', !input.prop('required'));
     };
     const fadeEl = el => {
         el.classList.add('fade');
         setTimeout(() => {
             el.classList.remove('fade');
         }, 1000);
-    }
+    };
     const action = target => {
         let el = 'courier' === target.val() ? deliveryYes : deliveryNo;
         fields.forEach(requiredToggle);
         toggleHidden(deliveryYes, deliveryNo);
         fadeEl(el);
-    }
-    let target
+    };
+    let target;
     methods.forEach(m => {
         if (m.checked) {
             target = $(m);
@@ -182,67 +180,6 @@ if (filterWrapper) {
     });
 }
 
-const shopList = document.querySelector('.shop__list');
-if (shopList) {
-    const addButtonOrderClickHandler = (shopOrder, form) => {
-        const buttonOrder = shopOrder.querySelector('.button');
-        const popupEnd = document.querySelector('.shop-page__popup-end');
-        buttonOrder.addEventListener('click', (evt) => {
-            form.noValidate = true;
-            const inputs = Array.from(shopOrder.querySelectorAll('[required]'));
-            inputs.forEach(inp => {
-                if (!!inp.value) {
-                    if (inp.classList.contains('custom-form__input--error')) {
-                        inp.classList.remove('custom-form__input--error');
-                    }
-                } else {
-                    inp.classList.add('custom-form__input--error');
-                }
-            });
-
-            if (inputs.every(inp => !!inp.value)) {
-                evt.preventDefault();
-                toggleHidden(shopOrder, popupEnd);
-                popupEnd.classList.add('fade');
-                setTimeout(() => popupEnd.classList.remove('fade'), 1000);
-                window.scroll(0, 0);
-                const buttonEnd = popupEnd.querySelector('.button');
-                buttonEnd.addEventListener('click', () => {
-                    popupEnd.classList.add('fade-reverse');
-                    setTimeout(() => {
-                        popupEnd.classList.remove('fade-reverse');
-                        toggleHidden(popupEnd, document.querySelector('.intro'), document.querySelector('.shop'));
-                    }, 1000);
-                });
-
-            } else {
-                window.scroll(0, 0);
-                evt.preventDefault();
-            }
-        });
-    };
-    shopList.addEventListener('click', (evt) => {
-        if (evt.target.classList.contains('product')) {
-            $(evt.target).data('link')
-        }
-
-        // const product = evt.path || (evt.composedPath && evt.composedPath());
-        // if (prod.some(pathItem => pathItem.classList && pathItem.classList.contains('shop__item'))) {
-        //     const shopOrder = document.querySelector('.shop-page__order');
-        //     toggleHidden(document.querySelector('.intro'), document.querySelector('.shop'), shopOrder);
-        //     window.scroll(0, 0);
-        //
-        //     shopOrder.classList.add('fade');
-        //     setTimeout(() => shopOrder.classList.remove('fade'), 1000);
-        //
-        //     const form = shopOrder.querySelector('.custom-form');
-        //     labelHidden(form);
-        //
-        //     toggleDelivery(shopOrder);
-        //     // addButtonOrderClickHandler(shopOrder, form)
-        // }
-    });
-}
 const shopOrder = document.querySelector('.shop-page__order');
 if (shopOrder) {
     toggleDelivery(shopOrder);
@@ -275,11 +212,7 @@ if (pageOrderList) {
 }
 
 const checkList = (list, btn) => {
-    if (list.children.length === 1) {
-        btn.hidden = false;
-    } else {
-        btn.hidden = true;
-    }
+    btn.hidden = list.children.length !== 1;
 };
 const addList = document.querySelector('.add-list');
 if (addList) {
@@ -290,15 +223,7 @@ if (addList) {
     const addInput = addList.querySelector('.product-photo-input');
 
     checkList(addList, addButton);
-    const addButtonOrderClickHandler = (form) => {
-        const button = document.querySelector('.button');
-        const popupEnd = document.querySelector('.page-add__popup-end');
-        button.addEventListener('click', (evt) => {
-            evt.preventDefault();
-            form.hidden = true;
-            popupEnd.hidden = false;
-        });
-    };
+
     addInput.addEventListener('change', evt => {
         const template = document.createElement('LI');
         const img = document.createElement('IMG');
@@ -322,9 +247,6 @@ if (addList) {
 
         reader.readAsDataURL(file);
     });
-
-    // addButtonOrderClickHandler(form);
-
 }
 
 const productsList = document.querySelector('.page-products__list');
@@ -341,7 +263,8 @@ if (productsList) {
 if (document.querySelector('.shop-page')) {
     const min = $('.min-price');
     const max = $('.max-price');
-    $('.range__line').slider({
+    const rangeLine = $('.range__line');
+    rangeLine.slider({
         min: min.data('limit'),
         max: max.data('limit'),
         values: [
@@ -349,13 +272,13 @@ if (document.querySelector('.shop-page')) {
             max.data('value'),
         ],
         range: true,
-        stop: function (event, ui) {
-            $('.min-price').text($('.range__line').slider('values', 0) + ' руб.');
-            $('.max-price').text($('.range__line').slider('values', 1) + ' руб.');
+        stop: function () {
+            min.text(rangeLine.slider('values', 0) + ' руб.');
+            max.text(rangeLine.slider('values', 1) + ' руб.');
         },
-        slide: function (event, ui) {
-            $('.min-price').text($('.range__line').slider('values', 0) + ' руб.');
-            $('.max-price').text($('.range__line').slider('values', 1) + ' руб.');
+        slide: function () {
+            min.text(rangeLine.slider('values', 0) + ' руб.');
+            max.text(rangeLine.slider('values', 1) + ' руб.');
         }
     });
 }
